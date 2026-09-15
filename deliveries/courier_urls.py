bed18af59bb8384c077b836f00699497d8a252b6
Software Engineer <65486194+Hardoffx@users.noreply.github.com>
2026-09-15 17:28:30 +0300

    Enable courier route reorder action

--- deliveries/courier_urls.py
+++ deliveries/courier_urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
  path('', views.courier_today, name='courier_today'),
  path('delivery/<int:pk>/update/', views.courier_update, name='courier_update'),
+ path('delivery/<int:pk>/reorder/', views.courier_reorder, name='courier_reorder'),
 ]
