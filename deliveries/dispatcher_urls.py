1ea359d09abc4e46d358f9f3648ab37df64cd81b
Software Engineer <65486194+Hardoffx@users.noreply.github.com>
2026-09-15 17:40:01 +0300

    Route inline delivery editing

--- deliveries/dispatcher_urls.py
+++ deliveries/dispatcher_urls.py
@@ -4,6 +4,7 @@ urlpatterns = [
  path('', views.dispatcher_dashboard, name='dispatcher_dashboard'),
  path('delivery/new/', views.delivery_create, name='delivery_create'),
  path('delivery/<int:pk>/edit/', views.delivery_edit, name='delivery_edit'),
+ path('delivery/<int:pk>/quick-edit/', views.dispatcher_quick_edit, name='dispatcher_quick_edit'),
  path('delivery/<int:pk>/assign/', views.dispatcher_assign, name='dispatcher_assign'),
  path('deliveries/bulk-assign/', views.dispatcher_bulk_assign, name='dispatcher_bulk_assign'),
  path('import/', views.import_excel, name='import_excel'),
