d953b682b495469b3ad72a5b6284985ff7909b59
Software Engineer <65486194+Hardoffx@users.noreply.github.com>
2026-09-16 00:55:11 +0300

    Avoid unnecessary user manager migration

--- accounts/models.py
+++ accounts/models.py
@@ -1,24 +1,13 @@
-from django.contrib.auth.models import AbstractUser, UserManager as DjangoUserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-
-class UserManager(DjangoUserManager):
-    def create_superuser(self, username, email=None, password=None, **extra_fields):
-        extra_fields.setdefault('role', User.Role.DISPATCHER)
-        extra_fields.setdefault('is_reserve_courier', False)
-        return super().create_superuser(username, email=email, password=password, **extra_fields)
-
 
 class User(AbstractUser):
     class Role(models.TextChoices):
         DISPATCHER = 'dispatcher', 'Диспетчер'
         COURIER = 'courier', 'Курьер'
-
     role = models.CharField(max_length=20, choices=Role.choices, default=Role.COURIER)
     phone = models.CharField(max_length=32, blank=True)
     is_reserve_courier = models.BooleanField(default=False, help_text='Резервный курьер без постоянного маршрута; может быть назначен на любой RouteRun')
-
-    objects = UserManager()
 
     @property
     def is_dispatcher(self):
