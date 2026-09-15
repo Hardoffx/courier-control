076f5abc60d363d13308a71a15a2f5968a226584
Software Engineer <65486194+Hardoffx@users.noreply.github.com>
2026-09-15 17:26:51 +0300

    Add delivery point directory to admin

--- deliveries/admin.py
+++ deliveries/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import Delivery, DeliveryEvent
+from .models import Delivery, DeliveryEvent, DeliveryPoint
+
+@admin.register(DeliveryPoint)
+class DeliveryPointAdmin(admin.ModelAdmin):
+    list_display = ('name','code','kind','address','is_active')
+    list_filter = ('kind','is_active')
+    search_fields = ('name','code','address','phone')
 
 @admin.register(Delivery)
 class DeliveryAdmin(admin.ModelAdmin):
-    list_display = ('delivery_date','address','courier','route_order','status','completed_at')
-    list_filter = ('delivery_date','status','courier')
-    search_fields = ('address','organization','recipient','phone')
+    list_display = ('delivery_date','source_label','address','courier','route_order','time_window','status','completed_at')
+    list_filter = ('delivery_date','status','courier','row_color')
+    search_fields = ('source_label','address','organization','recipient','phone')
 
 admin.site.register(DeliveryEvent)
