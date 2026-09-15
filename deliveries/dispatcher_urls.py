5da7550872b222b6fdcec6cd828186ba0e9ea543
Software Engineer <65486194+Hardoffx@users.noreply.github.com>
2026-09-15 17:45:42 +0300

    Route delivery point directory

--- deliveries/dispatcher_urls.py
+++ deliveries/dispatcher_urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from . import views, point_views
 urlpatterns = [
  path('', views.dispatcher_dashboard, name='dispatcher_dashboard'),
  path('delivery/new/', views.delivery_create, name='delivery_create'),
@@ -7,5 +7,9 @@ urlpatterns = [
  path('delivery/<int:pk>/quick-edit/', views.dispatcher_quick_edit, name='dispatcher_quick_edit'),
  path('delivery/<int:pk>/assign/', views.dispatcher_assign, name='dispatcher_assign'),
  path('deliveries/bulk-assign/', views.dispatcher_bulk_assign, name='dispatcher_bulk_assign'),
+ path('points/', point_views.point_list, name='point_list'),
+ path('points/new/', point_views.point_create, name='point_create'),
+ path('points/<int:pk>/edit/', point_views.point_edit, name='point_edit'),
+ path('points/<int:pk>/toggle/', point_views.point_toggle, name='point_toggle'),
  path('import/', views.import_excel, name='import_excel'),
 ]
