31b797a50206b217a59f8fb7b2d06f0d268925c3
Software Engineer <65486194+Hardoffx@users.noreply.github.com>
2026-09-15 17:15:58 +0300

    Expose Excel import endpoint

--- deliveries/dispatcher_urls.py
+++ deliveries/dispatcher_urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
  path('', views.dispatcher_dashboard, name='dispatcher_dashboard'),
  path('delivery/new/', views.delivery_create, name='delivery_create'),
  path('delivery/<int:pk>/edit/', views.delivery_edit, name='delivery_edit'),
+ path('import/', views.import_excel, name='import_excel'),
 ]
