5be5cad01e9da60ffe871d38a0dae6d3166a9e62
Software Engineer <65486194+Hardoffx@users.noreply.github.com>
2026-09-23 00:35:02 +0300

    Migrate generated delivery point codes back to blank

---
+++ deliveries/migrations/0006_allow_blank_point_codes.py
@@ -0,0 +1,26 @@
+from django.db import migrations, models
+
+
+def clear_generated_point_codes(apps, schema_editor):
+    DeliveryPoint = apps.get_model('deliveries', 'DeliveryPoint')
+    DeliveryPoint.objects.filter(code__startswith='point-').update(code='')
+
+
+class Migration(migrations.Migration):
+    dependencies = [('deliveries', '0005_courier_daily_note')]
+
+    operations = [
+        migrations.RemoveConstraint(
+            model_name='deliverypoint',
+            name='unique_point_code_address',
+        ),
+        migrations.AddConstraint(
+            model_name='deliverypoint',
+            constraint=models.UniqueConstraint(
+                fields=('code', 'address'),
+                condition=~models.Q(code=''),
+                name='unique_point_code_address',
+            ),
+        ),
+        migrations.RunPython(clear_generated_point_codes, migrations.RunPython.noop),
+    ]
