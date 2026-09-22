15c4c97a98f3ed9849ff01cc79e3516c648d8a5a
Software Engineer <65486194+Hardoffx@users.noreply.github.com>
2026-09-23 00:46:47 +0300

    Renumber delivery point migration after route run migration

---
+++ deliveries/migrations/0007_allow_blank_point_codes.py
@@ -0,0 +1,28 @@
+from django.db import migrations, models
+
+
+def clear_generated_point_codes(apps, schema_editor):
+    DeliveryPoint = apps.get_model('deliveries', 'DeliveryPoint')
+    DeliveryPoint.objects.filter(code__startswith='point-').update(code='')
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('deliveries', '0006_allow_multiple_route_runs_per_day'),
+    ]
+
+    operations = [
+        migrations.RemoveConstraint(
+            model_name='deliverypoint',
+            name='unique_point_code_address',
+        ),
+        migrations.AddConstraint(
+            model_name='deliverypoint',
+            constraint=models.UniqueConstraint(
+                fields=('code', 'address'),
+                condition=~models.Q(code=''),
+                name='unique_point_code_address',
+            ),
+        ),
+        migrations.RunPython(clear_generated_point_codes, migrations.RunPython.noop),
+    ]
