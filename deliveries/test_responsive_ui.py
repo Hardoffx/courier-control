cb552e1a3376b4be89f87fbd0a9c45ebc6bfa638
Software Engineer <65486194+Hardoffx@users.noreply.github.com>
2026-09-23 22:17:01 +0300

    Update responsive route editor tests

--- deliveries/test_responsive_ui.py
+++ deliveries/test_responsive_ui.py
@@ -15,21 +15,40 @@ class ResponsiveOperationalUiTests(TestCase):
         RouteTemplateItem.objects.create(template=self.template, point=self.point, route_order=1, time_window='09:00')
         self.client.login(username='ui-dispatcher', password='pass')
 
-    def test_route_template_has_phone_editing_controls(self):
+    def test_route_template_has_compact_mobile_editor_controls(self):
         response = self.client.get(reverse('route_detail', args=[self.route.pk]))
         self.assertEqual(response.status_code, 200)
         html = response.content.decode()
-        self.assertIn('template-mobile', html)
-        self.assertIn('mobile-template-actions', html)
-        self.assertIn('Сохранить время и комментарий', html)
-        self.assertIn('↑ Раньше', html)
-        self.assertIn('↓ Позже', html)
+        self.assertIn('admin-route-editor', html)
+        self.assertIn('inline-edit-form', html)
+        self.assertIn('Переместить', html)
+        self.assertIn('На позицию', html)
         self.assertIn('Убрать', html)
+        self.assertIn('Создать новую точку', html)
+        self.assertIn('name="phone"', html)
 
     def test_route_generation_controls_remain_on_same_screen(self):
         response = self.client.get(reverse('route_detail', args=[self.route.pk]))
-        self.assertContains(response, 'Создать маршрут на конкретный день')
-        self.assertContains(response, 'Что едет именно в этот день')
-        self.assertContains(response, 'Загрузить Excel в шаблон')
-        self.assertContains(response, 'Добавить точки вручную')
+        self.assertContains(response, 'Создать маршрут на день')
+        self.assertContains(response, 'В этот день')
+        self.assertContains(response, 'Excel')
+        self.assertContains(response, 'Добавить из базы')
         self.assertContains(response, self.point.address)
+
+    def test_new_point_can_be_created_directly_in_template(self):
+        response = self.client.post(reverse('template_create_point', args=[self.template.pk]), {
+            'kind': DeliveryPoint.Kind.CMD,
+            'code': '1370/4628/5180',
+            'name': 'Новая точка',
+            'address': 'Москва г, Митинская ул, дом № 17, корпус 4',
+            'phone': '+79990000000',
+            'time_window': '12:00-15:00',
+            'comment': 'Тест',
+            'enabled_by_default': '1',
+        })
+        self.assertEqual(response.status_code, 302)
+        point = DeliveryPoint.objects.get(code='1370/4628/5180')
+        self.assertEqual(point.address, 'Москва, ул Митинская 17к4')
+        item = RouteTemplateItem.objects.get(template=self.template, point=point)
+        self.assertEqual(item.time_window, '12:00-15:00')
+        self.assertEqual(item.comment, 'Тест')
